99f5176c98a40d548a28a645e4e36f3ba7042701
Ankit Raj <maurya.ubuntu@gmail.com>
2025-10-15 18:00:56 +0000

    add dark mode and light mode

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { ThemeToggle } from './ThemeToggle';
 
 const Footer = () => {
   return (
     <footer className="fixed bottom-4 right-4 z-50">
-      
+      <ThemeToggle />
     </footer>
   );
 };
