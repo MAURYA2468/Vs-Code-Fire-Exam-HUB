59af65b31eab14b4e06987f9be6fb5d190feae72
Ankit Raj <maurya.ubuntu@gmail.com>
2025-10-15 17:21:58 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/main/Sidebar.tsx
+++ src/components/main/Sidebar.tsx
@@ -40,15 +40,16 @@ export default function AppSidebar() {
         <SidebarMenu>
           {navItems.map((item) => (
             <SidebarMenuItem key={item.href}>
-              <Link href={item.href} legacyBehavior passHref>
-                <SidebarMenuButton
-                  isActive={pathname === item.href}
-                  className="w-full"
-                >
+              <SidebarMenuButton
+                asChild
+                isActive={pathname === item.href}
+                className="w-full"
+              >
+                <Link href={item.href}>
                   <item.icon className="h-4 w-4" />
                   <span>{item.label}</span>
-                </SidebarMenuButton>
-              </Link>
+                </Link>
+              </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
         </SidebarMenu>
